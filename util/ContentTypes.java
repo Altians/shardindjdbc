195251e0b71bd208d2cb1cd18bc8485c3417e2fa
chenruihui <18860389461@sina.cn>
2021-06-07 09:38:25 +0800

    日期，json，http工具类

---
+++ util/ContentTypes.java
@@ -0,0 +1,11 @@
+package com.example.demo.service.impl.util;
+
+public class ContentTypes {
+
+	public static final String application_x_www_form_urlencoded = "application/x-www-form-urlencoded";
+	public static final String application_json = "application/json";
+	public static final String text_plain = "text/plain";
+	public static final String application_xml = "application/xml";
+	public static final String text_html = "text/html";
+
+}
